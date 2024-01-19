af0d0a65ee46f75d9a86472ef843e38722536acc
ReeFLk <reeflk8@gmail.com>
2024-01-19 10:14:35 +0100

    First Version of The First Project

---
+++ src/components/MainContent.js
@@ -0,0 +1,7 @@
+export default function MainContent() {
+    return (
+        <main>
+            <h1 className="main--title">This is where most of my content will go...</h1>
+        </main>
+    );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import logo from "../logo.svg";
+export default function NavBar() {
+    return (
+        <nav>
+            <img src={logo} alt="logo" width="64px" />
+            <h3>React</h3>
+            <h4>Project 1</h4>
+        </nav>
+    );
+}
